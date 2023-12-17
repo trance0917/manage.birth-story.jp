e6f275df3f7d53fabffde216f79ba2302638e1e4
hachiya <kei8@apost.plala.or.jp>
2023-12-17 18:30:21 +0900

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import fs from 'fs'
 
 export default defineConfig({
     plugins: [
-        vue(),
         laravel({
-            input: ['resources/scss/app.scss' ,'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        host: true,
-        //ローカルのnpmでやりたい場合はこちら
-        //host: 'local-admin.e-metals.net',
-        port : 5201,
-        https:{
-            key: fs.readFileSync('/etc/letsencrypt/live/local.manage.birth-story.jp/privkey.pem'),
-            cert: fs.readFileSync('/etc/letsencrypt/live/local.manage.birth-story.jp/cert.pem'),
-        },
-        hmr: {
-            host: 'local.manage.birth-story.jp',
-        },
-        watch: {
-            usePolling: true,
-        },
-    },
 });
